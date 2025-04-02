9bc30384751481db13e6ddd2e418fa3b73f92932
benqbed <bcubed4321@gmail.com>
2025-04-02 17:50:33 -0600

    Add root url conf

--- cyberpunk_portfolio/settings.py
+++ cyberpunk_portfolio/settings.py
@@ -2,6 +2,8 @@ import os
 import dj_database_url
 from pathlib import Path
 
+ROOT_URLCONF = 'cyberpunk_portfolio.urls'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
